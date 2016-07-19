ace8dafbe634a199842047e0e3448c1aa402cfa0
Lisa Ballard <lballard.cat@gmail.com>
2016-07-19 13:35:07 -0700

    slight refactor

--- fetch.py
+++ fetch.py
@@ -11,7 +11,7 @@ with open(data_file, "r") as f:
     next(f)  # skip first line
     for line in f:
         img_id = re.compile("\s+").split(line)[1]  # split line by spaces
-        ring_obs_id = "S_IMG_CO_ISS_{}_N.json?cols=time1,timesec1".format(img_id.split('.')[0])
-        fullurl = metadata_url + ring_obs_id
+        ring_obs_id = "S_IMG_CO_ISS_{}_N.json".format(img_id.split('.')[0])
+        fullurl = metadata_url + ring_obs_id + '?cols=time1,timesec1'
         data = json.loads(requests.get(fullurl).text)
         print "%s,%s,%s" % (img_id, data[0]['time_sec1'], data[1]['time1'])
