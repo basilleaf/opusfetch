0c4eed17922482790493ca6c7666f9ad5cd8e67d
Lisa Ballard <lballard.cat@gmail.com>
2016-07-24 21:05:26 -0700

    bit improve hacky line

--- fetch.py
+++ fetch.py
@@ -9,7 +9,7 @@ metadata_url = base_url + '/metadata/'
 with open(data_file, "r") as f:
     next(f)  # skip first line
     for line in f:
-        img_id = re.compile("\s+").split(line)[1]  # split line by spaces
+        img_id = str(line.strip()[0:10])
         ring_obs_id = "S_IMG_CO_ISS_{}_N.json".format(img_id.split('.')[0])
         fullurl = metadata_url + ring_obs_id + '?cols=time1,timesec1'
         data = json.loads(requests.get(fullurl).text)
